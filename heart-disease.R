81cd1dc22e7fd7e206bdaa80c2effa32276bcda3
Michael <wathuomichael@gmail.com>
2025-04-22 13:43:36 +0300

    eda changes

--- heart-disease.R
+++ heart-disease.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(caTools)
 library(car)
 
-heart.data <- read.csv('./Documents/stats/models/Logistic Regression/Heart-disease-classification/framingham.xls')
+heart.data <- read.csv('./Documents/stats/models/logistic-regression/Heart-disease-classification/framingham.xls')
 
 heart.data$male <- sapply(heart.data$male, factor)
 heart.data$education <- sapply(heart.data$education, factor)
@@ -18,16 +18,34 @@ missmap(heart.data, col = c('yellow', 'black'))
 
 heart.data <- na.omit(heart.data)
 
-ggplot(heart.data, aes(as.factor(TenYearCHD), age)) + geom_boxplot() + theme_bw()
-ggplot(heart.data, aes(x = factor((TenYearCHD), labels = c('No Disease', 'Heart Disease')), y = age)) + 
+#age against risk boxplot
+ggplot(heart.data, aes(x = factor((TenYearCHD), labels = c('No', 'Yes')), age)) +
+  geom_boxplot() + theme_bw() + 
+  xlab('Heart Disease Risk') + ylab('Age') +
+  ggtitle('Age against Heart Disease Risk') + 
+  theme(plot.title = element_text(hjust = 0.5))
+
+#age against risk violin plot
+ggplot(heart.data, aes(x = factor((TenYearCHD), labels = c('No', 'Yes')), y = age)) + 
   geom_violin(aes(fill = TenYearCHD)) + theme_bw() +
   ggtitle('Age against Heart Disease Risk') + 
-  xlab('Heart disease risk') + ylab('Age') +
+  xlab('Heart Disease Risk') + ylab('Age') +
   scale_fill_discrete(labels = c('No Disease', 'Heart Disease')) +
   theme(plot.title = element_text(hjust = 0.5))
 
-table(heart.data$TenYearCHD, heart.data$currentSmoker, dnn = c('TenYearCHD', 'currentsmoker'))
-table(heart.data$TenYearCHD, heart.data$BPMeds, dnn = c('TenYearCHD', 'bpmeds'))
+#sysbp against risk boxplot
+ggplot(heart.data, aes(factor((TenYearCHD), labels = c('No', 'Yes')), sysBP)) + 
+  geom_boxplot() + theme_bw() + 
+  xlab('Heart Disease Risk') + ylab('Systolic Blood Pressure') +
+  ggtitle('Systolic Blood Pressure against Heart Disease Risk') +
+  theme(plot.title = element_text(hjust = 0.5))
+
+#diabp against risk boxplot
+ggplot(heart.data, aes(factor((TenYearCHD), labels = c('No', 'Yes')), diaBP)) + 
+  geom_boxplot() + theme_bw() + 
+  xlab('Heart Disease Risk') + ylab('Diastolic Blood Pressure') +
+  ggtitle('Diastolic Blood Pressure against Heart Disease Risk') +
+  theme(plot.title = element_text(hjust = 0.5))
 
 set.seed(0)
 
